3484e7f0f446e3fc346561bf03024632baf1db81
yuzhen1024 <47154428+yuzhen1024@users.noreply.github.com>
2024-01-27 04:30:24 +0800

    Update speedtoggle.lua

--- speedtoggle.lua
+++ speedtoggle.lua
@@ -1,6 +1,7 @@
 control_key = "z"
-save_speed = mp.get_property("speed")
-function speed_toggle(n)
+local save_speed = mp.get_property("speed")
+mp.add_key_binding(control_key, "speed_toggle", function()
+	local n = mp.get_property("speed")
 	if tonumber(n)==1 then
 		mp.set_property("speed", save_speed)
 	elseif  n~=1 then
@@ -9,5 +10,4 @@ function speed_toggle(n)
 	end
 	msg = string.format("%.2f",mp.get_property("speed"))
 	mp.osd_message("Speed: "..msg)
-end
-mp.add_key_binding(control_key, "speed_toggle", function() speed_toggle(mp.get_property("speed")) end)
+end)
