c930f9307c8e536c072b97a84fdbb3ca7f8c6433
yuzhen1024 <47154428+yuzhen1024@users.noreply.github.com>
2024-06-10 22:54:19 +0800

    Update speedtoggle.lua
    Bind more keys

--- speedtoggle.lua
+++ speedtoggle.lua
@@ -1,14 +1,21 @@
 local mp = require('mp')
-local control_key = "z"
-local save_speed = mp.get_property("speed")
-mp.add_key_binding(control_key, "speed_toggle", function()
+
+local keys = {"z", }  -- append to `,` bind the key you want
+
+local temp_speed = mp.get_property("speed")
+
+local function update()
 	local n = mp.get_property("speed")
 	if tonumber(n)==1 then
-		mp.set_property("speed", save_speed)
+		mp.set_property("speed", temp_speed)
 	elseif  n~=1 then
-		save_speed = mp.get_property("speed")
+		temp_speed = mp.get_property("speed")
 		mp.set_property("speed", 1)
 	end
 	local msg = string.format("%.2f",mp.get_property("speed"))
 	mp.osd_message("Speed: "..msg)
-end)
+end
+
+for i, v in pairs(keys) do
+	mp.add_key_binding(v, string.format("speed_toggle_%s", i), update)
+end
